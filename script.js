657f850cd990ae10783f4a5179b654ef198c9cbc
aditiyakamodiya <aditiyakamodiya@gmail.com>
2023-11-11 16:16:27 +0530

    .

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ function randomnumbers() {
         // if (array.length < 11) {
         //     randomnumbers()
         // }
-        if(array.length ==10){
+        if (array.length == 10) {
 
             console.log(array)
         }
         else
-        randomnumbers()
+            randomnumbers()
     }
     else {
         randomnumbers()
@@ -26,20 +26,21 @@ function randomnumbers() {
 
 }
 
-function showoption(){
-for(let j=0;j<option.length;j++){
- option[j].innerHTML = array[j]
-}}
+function showoption() {
+    for (let j = 0; j < option.length; j++) {
+        option[j].innerHTML = array[j]
+    }
+}
 
 
 
 let interval = setInterval(() => {
     if (i == 2) {
-let randomshow = Math.floor(Math.random()*10)
-         
+
+        let randomshow = Math.floor(Math.random() * 10)
 
         loading.innerHTML = randomshow
-        
+
         showoption()
         clearInterval(interval)
 
